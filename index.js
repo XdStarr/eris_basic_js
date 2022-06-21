4923c27f67b348aa99a3545d94bcecefc6945aea
XdStarr <54508663+XdStarr@users.noreply.github.com>
2022-06-20 19:27:15 -0700

    Added Basic Bot Code

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, Collection, Constants } = require("eris");
+
+const env = require("dotenv").config();
+
+const client = new Client(process.env.TOKEN, {
+    intents: [
+        "guildMessages",
+        "guildPresences",
+        "guildMembers",
+        "guilds"
+    ],
+    allowedMentions: { repliedUser: true },
+    getAllUsers: true,
+    autoreconnect: true,
+    maxShards: "auto"
+});
+
+client.commands = new Collection();
+client.cooldowns = new Collection();
+
+client.connect();
